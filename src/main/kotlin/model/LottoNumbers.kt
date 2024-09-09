3facb22574e0e878e9cde0c00c90925cd7624c40
jingal <ilovejinjoo@gmail.com>
2024-09-09 14:50:23 +0900

    feat : 로토복권 숫자 조합 및 번호가 정상인지 확인

--- src/main/kotlin/model/LottoNumbers.kt
+++ src/main/kotlin/model/LottoNumbers.kt
@@ -2,4 +2,15 @@ package model
 
 class LottoNumbers(
     val numbers: List<Int>
-)
+) {
+    init {
+        require(numbers.size == 6) {
+            "[ERROR] 정상적인 로토 번호 조합이 아닙니다."
+        }
+
+
+        require (numbers.all { it in 1..45}) {
+            "[ERROR] 정상적인 로토 번호가 아닙니다."
+        }
+    }
+}
