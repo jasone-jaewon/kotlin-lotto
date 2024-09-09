8e3ffd102da48f7b8d125bee30d387c340332018
jaewon.jung <jgd0948@naver.com>
2024-09-09 14:34:31 +0900

    feat: calculateLotteryGames 로직 구현

--- src/main/kotlin/LottoUtil.kt
+++ src/main/kotlin/LottoUtil.kt
@@ -1,3 +1,9 @@
-fun calculateLotteryGames(money: Int) : Int{
-    TODO()
+private const val DIVIDE_UNIT = 1000
+private const val ZERO = 0
+fun calculateLotteryGames(money: Int): Int {
+    require(money >= ZERO) {
+        "입력한 금액은 양수여야 합니다."
+    }
+
+    return money / DIVIDE_UNIT
 }
