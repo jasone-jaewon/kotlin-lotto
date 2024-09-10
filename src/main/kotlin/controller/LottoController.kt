20ef3869649454de8dbb8ac99b46200cfc5c9ec4
jingal <ilovejinjoo@gmail.com>
2024-09-10 13:32:01 +0900

    refactor : createLotto refactoring

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -4,12 +4,7 @@ import generateLottoNumbers
 import model.Lotto
 
 class LottoController {
-
     fun createLotto(games: Int): List<Lotto> {
-        return buildList {
-            for (n in 1..games) {
-                add(Lotto(generateLottoNumbers()))
-            }
-        }
+        return List(games) { Lotto(generateLottoNumbers()) }
     }
 }
